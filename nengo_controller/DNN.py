be269416da0afbc5cef59d459b7f2bf074f68f8d
Nimar <l.nimar.b@gmail.com>
2018-01-08 10:17:20 +0100

    Add: basic prediction working

--- nengo_controller/DNN.py
+++ nengo_controller/DNN.py
@@ -1,12 +1,26 @@
+import os
+
+import numpy as np
+import cv2
+from keras.models import load_model
+from cv_bridge import CvBridge
+#from sensor_msgs.msg import Image
+
+CURRENT_DIR = os.path.abspath(os.path.dirname(__file__))
+MODEL_DIR = os.path.join("..", "src", "models")
+
+
 class DNN:
-    def __init__(self):
+    def __init__(self, img_w=80, img_h=60, model_name="learndrive-model-89752"):
         """
 
         """
-        _DNN = self.load_dnn()
-
-    def load_dnn(self):
-        return None
+        self._bridge = CvBridge()
+        self._model_name = model_name
+        self._model = load_model(os.path.join(MODEL_DIR, self._model_name))
+        self._img_width = img_w
+        self._img_height = img_h
+        self._img_resize_factor = self._img_width / 640
 
     def propagate(self, img):
         """
@@ -14,6 +28,21 @@ class DNN:
         :param img: img is in raw format. sensor_msgs.msg Image
         :return:
         """
-        angle = 1
-        displacement = 1
+        cvimg = self._bridge.imgmsg_to_cv2(img, "bgr8")
+        cvimg = cv2.resize(cvimg,
+                            None,
+                            fx=self._img_resize_factor,
+                            fy=self._img_resize_factor)
+
+        data = np.empty(
+                (1, self._img_height, self._img_width, 3),
+                dtype=object)
+        data[0, :, :, :] = cvimg
+
+        prediction = self._model.predict(x=data, batch_size=1)
+
+        angle = prediction[0]
+        displacement = prediction[1]
+        print("Pred ang: " + str(angle) + "; dis: " + str(displacement))
+
         return angle, displacement
